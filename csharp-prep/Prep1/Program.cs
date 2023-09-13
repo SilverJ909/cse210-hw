fc174c8dbff9d6559729f4313f8ec6eee3898d8b
SilverJ909 <joshua.c.flood@gmail.com>
2023-09-13 06:26:22 -0400

    I made the program get first and last name and display it James Bond style.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Get First Name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        //Get Last Name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        //Display the first and last name as "Your name is last_name, first_name last_name
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
