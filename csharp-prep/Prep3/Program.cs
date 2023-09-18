9181fe74a71f47e4cf87ccf8100b864d94881b0d
SilverJ909 <joshua.c.flood@gmail.com>
2023-09-18 19:06:31 -0400

    Made a Guessing Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+        string g = null;
+        int guesses = 0;
+        string toPlay = "yes";
+
+        while (toPlay.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1,100);
+            guess = 0;
+            guesses = 0;
+            Console.WriteLine("Guess the magic number");
+
+
+            while (guess != magicNum)
+            {
+                Console.Write("What is your guess? ");
+                g = Console.ReadLine();
+                guess = int.Parse(g);
+
+                if (guess == magicNum)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (guess < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+                guesses++;
+            }
+            Console.WriteLine($"It took you {guesses} guesses.");
+
+            Console.Write("Play again? ");
+            toPlay = Console.ReadLine();
+        }
     }
 }
