0aad4539ea890d8f616ffb97f79805cdf928d202
SilverJ909 <joshua.c.flood@gmail.com>
2023-09-25 18:52:39 -0400

    Added a _name to Resume object

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -17,6 +17,7 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
+        myResume._name = "Joshua Flood";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
