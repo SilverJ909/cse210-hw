23142b0cad9fad75712a4b83a6a2fb79df906687
SilverJ909 <joshua.c.flood@gmail.com>
2023-10-10 17:25:16 -0400

    Made Fraction Program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        List<Fraction> fractions = new List<Fraction>();
+        Fraction firstFraction = new Fraction();
+        Fraction secondFraction = new Fraction(5);
+        Fraction thirdFraction = new Fraction(1, 3);
+
+        firstFraction.SetTop(3);
+        firstFraction.SetBottom(5);
+
+        fractions.Add(firstFraction);
+        fractions.Add(secondFraction);
+        fractions.Add(thirdFraction);
+
+        foreach (Fraction i in fractions)
+        {
+            Console.WriteLine($"Numerator: {i.GetTop()}");
+            Console.WriteLine($"Denominator: {i.GetBottom()}");
+            Console.WriteLine($"Fraction: {i.GetFractionString()}");
+            Console.WriteLine($"Decimal: {i.GetDecimalValue()}\n");
+        }
     }
 }
