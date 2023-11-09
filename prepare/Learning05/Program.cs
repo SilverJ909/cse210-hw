c9e603fa1b00591aff05c8dfb11ddcf6e3fcd70d
SilverJ909 <joshua.c.flood@gmail.com>
2023-11-09 15:05:04 -0500

    Polymorphism Learning

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Blue", 5);
+        Rectangle rectangle = new Rectangle(2.5, 12, "Pink");
+        Circle circle = new Circle(6, "Green");
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape i in shapes)
+        {
+            Console.WriteLine($"Color: {i.GetColor()}");
+            Console.WriteLine($"Area: {i.GetArea()}");
+        }
     }
 }
